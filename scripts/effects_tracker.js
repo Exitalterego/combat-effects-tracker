ae71a086b78f71d84ab026cf9194ec42a659f3ba
Mike Kamysz <mike.kamysz@outlook.de>
2020-07-15 16:49:01 +0200

    Make it work baby

--- scripts/effects_tracker.js
+++ scripts/effects_tracker.js
@@ -1,3 +1,14 @@
-Hooks.on("game.combat.nextTurn", function() {
-    console.log("Does this work?");
+Hooks.on("updateCombat", function() {
+    // Execute function once updateCombat Hook is called (e.g. after progressing to next turn)
+    
+    let c = game.combat.combatant
+
+    if (c.name.toLowerCase().includes("[effect]")) {
+        let token = canvas.tokens.ownedTokens.filter(el => el.actor.data._id == c.actor.data._id)[0];
+
+        ac = token.data.actorData.data.attributes.ac.value;
+        ac = ac - 1;
+        
+        token.update({"actorData.data.attributes.ac.value": ac});
+    }
 });
