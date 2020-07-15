5d32248636a34c87083e766d3dcf1b180625de56
Mike Kamysz <mike.kamysz@outlook.de>
2020-07-15 16:58:28 +0200

    Update effects_tracker.js

--- scripts/effects_tracker.js
+++ scripts/effects_tracker.js
@@ -6,7 +6,7 @@ Hooks.on("updateCombat", function() {
     if (c.name.toLowerCase().includes("[effect]")) {
         let token = canvas.tokens.ownedTokens.filter(el => el.actor.data._id == c.actor.data._id)[0];
 
-        ac = token.data.actorData.data.attributes.ac.value;
+        ac = token.actor.data.data.attributes.ac.value;
         ac = ac - 1;
         
         token.update({"actorData.data.attributes.ac.value": ac});
