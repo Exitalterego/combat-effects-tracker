4d1f74623cc62750505fb1f1a8da2e05dadc2042
Mike Kamysz <mike.kamysz@outlook.de>
2020-07-17 19:33:02 +0200

    Testing new version

--- scripts/settings.js
+++ scripts/settings.js
@@ -13,23 +13,14 @@ Hooks.on("init", () => {
         option_choices[item] = item;
     });
 
-    game.settings.register("combat-effects-tracker", "effectsPrefix", {
-          name: "Combat Effect Prefix",
-      hint: "Specify the prefix in the name of a combatant that identifies that its tracked resource shall be lowered on each of its turns.",
-          scope: "world",
-          config: true,
-          default: true,
-          type: Boolean
-      });
-    
-      game.settings.register("combat-effects-tracker", "trackedResource", {
-          name: "Tracked resource",
-          hint: "Specify which resource you track in your combat trackers.",
-          scope: "world",
-          config: true,
-          default: 1,
-      type: String,
-      choices: option_choices
-      });  
+    game.settings.register("combat-effects-tracker", "trackedResource", {
+        name: "Tracked resource",
+        hint: "Specify which resource you track in your combat trackers.",
+        scope: "world",
+        config: true,
+        default: 1,
+    type: String,
+    choices: option_choices
+    });  
 
 });
