09faa1a1670e7a3a9201719a42d2110e2ce039db
Mike Kamysz <mike.kamysz@outlook.de>
2020-07-17 19:14:15 +0200

    Update settings.js

--- scripts/settings.js
+++ scripts/settings.js
@@ -1,5 +1,18 @@
-Hooks.once("init", () => {
-    
+Hooks.on("init", () => {
+    let ct_config = new CombatTrackerConfig();
+    let attr_choices = ct_config.getAttributeChoices();
+    let arr = [];
+
+    Object.keys(attr_choices).forEach(function(item, index) {
+        arr = arr.concat(attr_choices[item]);
+    });
+
+    let option_choices = {};
+
+    arr.forEach(function(item, index) {
+        option_choices[item] = item;
+    });
+
     game.settings.register("combat-effects-tracker", "effectsPrefix", {
           name: "Combat Effect Prefix",
       hint: "Specify the prefix in the name of a combatant that identifies that its tracked resource shall be lowered on each of its turns.",
@@ -16,8 +29,7 @@ Hooks.once("init", () => {
           config: true,
           default: 1,
       type: String,
-      choices: {test: "test", na: "na?", holdup: "hää"},
-      range: {min: 0, max: 50, step: 0.5}
+      choices: option_choices
       });  
 
 });
